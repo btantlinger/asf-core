bf0b03431f8ab450861e57cd0e57d323d526b581
Bob Tantlinger <bob.tantlinger@gmail.com>
2014-05-08 21:25:06 -0400

    Modified worker.php to point to composer autoload.php

--- worker.php
+++ worker.php
@@ -16,12 +16,19 @@ $queue = $argv[1];
 //we're running from the cli, so base url does not make sense
 define('BASE_URL', "file://" . SRV_ROOT);
 
-//load the asf lib
-require 'bootstrap.php';
-try {
-    \asf\job\Worker::getInstance()->startWorker($queue);
-} catch(\Exception $ex) {
-    echo $ex->getMessage() . PHP_EOL;
+//we need to include composer autoload.php here so that auto include is
+//available to the cli script
+$path = dirname(dirname(dirname(__FILE__))); // should be the vendor path;
+$composerAutoloader = $path . DIRECTORY_SEPARATOR . "autoload.php";
+if(file_exists($composerAutoloader) && is_file($composerAutoloader)) {
+    require $composerAutoloader;
+    try {
+        \asf\job\Worker::getInstance()->startWorker($queue);
+    } catch(\Exception $ex) {
+        echo $ex->getMessage() . PHP_EOL;
+        exit(0);
+    }
+} else {    
     exit(0);
 }
 //EOF
